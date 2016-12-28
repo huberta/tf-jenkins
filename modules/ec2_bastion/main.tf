e4b980f6d1b25d928b034e5ec404cdf3d97d7b1f
Rob Hough <robert.hough@nextgearcapital.com>
2016-12-27 21:41:10 -0500

    ec2_bastion was broke

--- modules/ec2_bastion/main.tf
+++ modules/ec2_bastion/main.tf
@@ -9,8 +9,9 @@ data "template_file" "bastion_user_data" {
 ### Create our bastion instance(s)
 resource "aws_instance" "ec2_bastion" {
     ami = "${var.ami_id}"
+    count = "${var.count}"
     vpc_security_group_ids = ["${split(",", var.security_group_id)}"]
-    subnet_id = "${var.subnet_id}"
+    subnet_id = "${element(split(",", var.subnet_id), count.index%2)}"
     instance_type = "t2.micro"
     key_name = "devops.id_rsa"
     associate_public_ip_address = true
@@ -21,4 +22,13 @@ resource "aws_instance" "ec2_bastion" {
     }
 
     user_data = "${data.template_file.bastion_user_data.rendered}"
+}
+
+### Create/Update our Route53 record with the bastion IP
+resource "aws_route53_record" "bastion" {
+   zone_id = "Z3LIF1DTMK97J9"
+   name = "${var.name}.dev.nextgearcapital.com"
+   type = "A"
+   ttl = "300"
+   records = ["${aws_instance.ec2_bastion.public_ip}"]
 }
